b23dc840386462c153c2e15c45ab6ab34585242b
zy383 <zy383@cornell.edu>
2024-09-08 13:49:55 -0400

    finished implementation

--- PairTripleDetectorGL.v
+++ PairTripleDetectorGL.v
@@ -17,6 +17,15 @@ module PairTripleDetectorGL
   // Implement pair/triple detector using explicit gate-level modeling
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+wire w;
+wire x;
+wire y;
+
+or(w, in0, in1);
+and(x, in0, in1);
+and(y, w, in2);
+or(out, x, y);
+
 endmodule
 
 `endif /* PAIR_TRIPLE_DETECTOR_GL_V */
